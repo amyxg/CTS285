a70e9486d161af0914e3b04717663ea954f545a1
Amy Santjer <101990402+amyxg@users.noreply.github.com>
2024-08-31 19:16:49 +0000

    changes to math functions

--- Calculator Python/formulaFunctions.py
+++ Calculator Python/formulaFunctions.py
@@ -35,44 +35,86 @@ def getIntegerInput(prompt):
 
 # determine decision structure for menu choice
 def mathOption(userChoice):
-    match userChoice:
-        case 1:
-            add(num1, num2)
-        case 2: 
-            subtract(num1, num2)
-        case 3:
-            divide(num1, num2)
-        case 4:
-            multiply(num1, num2)
-        case 5:
-            print("Goodbye...")    
-        case _:
-            # this is the default handler if none of the abvoe cases match
-            print("Invalid option! Please enter a number from 1 to 5.")
+    while True:
+        match userChoice:
+            case 1:
+                print("Add")
+                num1 = getIntegerInput("Enter a number: ")
+                num2 = getIntegerInput("Enter a number: ")
+                print(f"{num1} + {num2} = {add(num1, num2)}")
+            case 2: 
+                print("Subtract")
+                num1 = getIntegerInput("Enter a number: ")
+                num2 = getIntegerInput("Enter a number: ")
+                print(f"{num1} + {num2} = {subtract(num1, num2)}")
+            case 3:
+                print("Divide")
+                num1 = getIntegerInput("Enter a number: ")
+                num2 = getIntegerInput("Enter a number: ")
+                print(f"{num1} + {num2} = {divide(num1, num2)}")
+            case 4:
+                print("Multiply")
+                num1 = getIntegerInput("Enter a number: ")
+                num2 = getIntegerInput("Enter a number: ")
+                print(f"{num1} + {num2} = {multiply(num1, num2)}")
+            case 5:
+                print("Goodbye...")    
+                return
+            case _:
+                # this is the default handler if none of the above cases match
+                print("Invalid option! Please enter a number from 1 to 5. ")
+                return
+        # exit match case if all are valid     
+        return 
 
 # math formulas
 def add(num1, num2):
+    """
+    num1: int 
+    num2: int
+    """
     return num1 + num2
 
 def subtract(num1, num2):
+    """
+    num1: int 
+    num2: int
+    """
     return num1 - num2
 
 def divide(num1, num2):
+    """
+    num1: int 
+    num2: int
+    """
+    if num2 == 0:
+        return "Cannot divide by zero"
     return num1 / num2
 
 def multiply(num1, num2):
+    """
+    num1: int 
+    num2: int
+    """
     return num1 * num2
 
 # second menu to determine if user wish to repeat or redirect to menu
 def secondaryMenu(userChoice, userSecChoice):
-    print("1. Repeat")
-    print("2. Main Menu")
-    match userSecChoice:
-        case 1:
-            mathOption(userChoice)
-            print("repeat option")
-        case 2: 
-            displayMenu()  
-        case _:
-            # this is the default handler if none of the abvoe cases match
-            print("ERROR! PLease enter a number from 1 to 2. ")
+    """
+    userChoice: int 
+    userSecChoice: int
+    """
+    while True: 
+        print("1. Repeat")
+        print("2. Main Menu")
+        userSecChoice = getIntegerInput("Enter a number: ")
+        
+        match userSecChoice:
+            case 1:
+                print("repeat option selected ")
+                mathOption(userChoice)
+            case 2: 
+                displayMenu()  
+            case _:
+                # this is the default handler if none of the abvoe cases match
+                print("ERROR! PLease enter a number from 1 to 2. ")
--- Calculator Python/m1hw_Santjer.py
+++ Calculator Python/m1hw_Santjer.py
@@ -2,14 +2,14 @@
 import formulaFunctions as f
 
 def main():
-    f.displayMenu()
-    userInput = f.getIntegerInput("Enter a number: ")
-    f.mathOption(userInput)
-    firstNum = f.getIntegerInput("Enter a number: ")
-    secondNum = f.getIntegerInput("Enter a number: ")
+    while True: 
+        f.displayMenu()
+        userInput = f.getIntegerInput("Enter a number: ")
+        f.mathOption(userInput)
+        if userInput == 5:
+            break
+        f.secondaryMenu()
 
-
-    
 
 
 if __name__ == "__main__":
