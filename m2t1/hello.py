889be1d48bbe7a8a4429a91cb76ce4fed8735054
Amy Santjer <101990402+amyxg@users.noreply.github.com>
2024-09-11 16:31:09 +0000

    added sec route (action)

--- m2t1/hello.py
+++ m2t1/hello.py
@@ -4,5 +4,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h3>Hello, World!</h3>"
+def index():
+    # three quotes for multiline strings
+    return """
+    <h3>Hello, World!</h3>
+    <p>This is a paragraph</p>
+    <a href="action">Click Here</a>
+
+    """
+@app.route("/action")
+def action():
+    return "Hello from the action route!"
