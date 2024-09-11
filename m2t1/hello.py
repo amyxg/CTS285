ffc614eff0153d6ff0f3e2acb4d01a75a4efcceb
Amy Santjer <101990402+amyxg@users.noreply.github.com>
2024-09-11 16:47:47 +0000

    added template variable

--- m2t1/hello.py
+++ m2t1/hello.py
@@ -1,5 +1,5 @@
 # minimal Flask app
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 # Do any app specific setup here
@@ -7,13 +7,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # three quotes for multiline strings
-    return """
-    <h3>Hello, World!</h3>
-    <p>This is a paragraph</p>
-    <a href="action">Click Here</a>
+    # find the template in /templates
+    name = "Amy"
+    return render_template("main_page.html", name=name)
 
-    """
 @app.route("/action")
 def action():
     return "Hello from the action route!"
