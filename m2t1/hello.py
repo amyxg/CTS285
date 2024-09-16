a5fdd76853ef12977729abb9de92b7f79faa563d
Amy Santjer <101990402+amyxg@users.noreply.github.com>
2024-09-16 16:58:35 +0000

    post comments deets

--- m2t1/hello.py
+++ m2t1/hello.py
@@ -1,16 +1,20 @@
 # minimal Flask app
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 # Do any app specific setup here
 # for instance, loading a database
 app.config["DEBUG"] = True # will run in debug mode
+#ro: make thiis a databasee or something
+comments = []
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"]) # we allow posting
 def index():
-    # find the template in /templates
-    #name = "Amy"
-    return render_template("main_page.html")
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+    #otherwise, it's a post
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
 
 @app.route("/action")
 def action():
