f32ee2035e2a1797b18dd0e3947212d93e4b7a0e
Amy Santjer <101990402+amyxg@users.noreply.github.com>
2024-10-09 16:34:05 +0000

    create login page

---
+++ Login/userLogin.py
@@ -0,0 +1,21 @@
+# This program logs in users: students, parents, teachers
+
+def getUsername():
+    username = input("Enter username: ")
+
+
+def getPasswd():
+    passwd = input("Enter password: ")
+
+def displayErrorMsg():
+    print("Error! input username and/or password does not exist.")
+    print("Check spelling and ")
+
+def main():
+    print("Test main")
+
+
+
+
+if __name__ == "__main__":
+    main()
